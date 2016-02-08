7b655b39240f40cff5b7d6c16e9eb31035bb910a
Melville Ezeogu <Melville.Ezeogu@cfpb.gov>
2016-02-08 18:06:49 -0500

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 
 sudo yum -y groupinstall 'Development Tools'
-sudo yum -y install java-1.7.1*
-sudo yum -y install vim
-sudo yum -y install yum localinstall http://yum.postgresql.org/9.4/redhat/rhel-6-x86_64/pgdg-centos94-9.4-1.noarch.rpm
-sudo yum -y install postgresql9*
-sudo yum -y install tree
 wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
 sudo rpm -ivh epel-release-latest-6.noarch.rpm
 sudo yum -y install protobuf-c-devel   #requirement
@@ -13,7 +8,7 @@ sudo yum -y install protobuf-c-devel   #requirement
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
-if [ "$SCRIPTPATH" = "/Users/ezeogum/Projects/rpm-git-clones/cstore_fdw" ] ; then
+if [ "$SCRIPTPATH" = "/tmp" ] ; then
        SCRIPTPATH=/vagrant
    fi
   
